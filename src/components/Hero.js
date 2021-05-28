85496524bb4e2c158e130c6a359a5b6010343cbd
jejaksaid <said@fastlearningcenter.com>
2021-05-28 23:52:54 +0700

    hero and hero image query(error)

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,10 +3,44 @@ import Image from "gatsby-image"
 import { Link } from "gatsby"
 import { graphql, useStaticQuery } from "gatsby"
 import SocialLinks from "../constants/socialLinks"
-// ...GatsbyImageSharpFluid
+
+const query = graphql`
+  {
+    file(relativePath: { eq: "hero-img.png" }) {
+      childrenImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
 
 const Hero = () => {
-  return <h2>hero component</h2>
+  // const {
+  //   file: {
+  //     childImageSharp: { fluid },
+  //   },
+  // } = useStaticQuery(query)
+
+  return (
+    <header className="hero">
+      <div className="section-center hero-center">
+        <article className="hero-info">
+          <div>
+            {/* <Image fluid={fluid} className="hero-img" /> */}
+            <div className="underline"></div>
+            <h1>saidramdhan</h1>
+            <h4>freelance web developer</h4>
+            <Link to="/contact" className="btn">
+              contact me
+            </Link>
+            <SocialLinks />
+          </div>
+        </article>
+      </div>
+    </header>
+  )
 }
 
 export default Hero
